4dac42752700c897be9ace43771baf1438855cb8
arpitaverma28 <arpitaverma13578@gmail.com>
2026-03-15 02:08:49 +0530

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const id =1122
+//id = 66 //not allowed
+console.log(id);
+
+let accountEmail ="arpita@123gmail.com"
+var  accountPassword="123"
+accountCity="Delhi"
+console.table([accountEmail,accountPassword,accountCity])
+  
+accountEmail ="rashi123@gmail.com"
+accountPassword="678"
+accountCity="Hydrabad"
+let accountState
+console.table([id,accountEmail,accountPassword,accountCity,accountState])
