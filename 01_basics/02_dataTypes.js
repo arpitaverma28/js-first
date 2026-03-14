587db87887e75960f2f3e1261fa694c7cdcb04a1
arpitaverma28 <arpitaverma13578@gmail.com>
2026-03-15 02:07:03 +0530

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,50 @@
+"user strict" // treat all js code as newer version
+//alert(3+3) // we are using node js not browser
+
+//primitive data type 
+ 
+//number : use for number
+let age = 20;
+let price=199.99;
+
+//string :user for text
+let name = "Arpita";
+
+//boolean : for true or false value
+let isStudent = true;
+
+//undefined : a variable decleared but not assigned a value
+let x;
+console.log(x); 
+
+//null : for represents empty or no value
+let data = null;
+
+//BigInt : for large number
+let bigNumber = 68268009463n;
+
+//Symbol : used to create unique identifiers
+let id = Symbol("id");
+ 
+//  Non-primitive (Reference) data type
+
+//Object : store multiple values together
+let student ={
+    name: "Rashi",
+    age:20,
+    city:"Barabanki"
+};
+
+//Array : store multiple values is a list
+let fruit =["Apple","Mango","Banana"];
+
+//funtion : funtions are also treated as objects
+function greet(){
+    console.log("Hi");
+}
+
+
+//for type 
+console.log(typeof"Rashi");
+console.log(typeof age);
+console.log(typeof null); //returns Object
