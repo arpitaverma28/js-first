ce2fd0b1002eafa514271c3c15c1f7d3f4d3d9a9
arpitaverma28 <arpitaverma13578@gmail.com>
2026-03-15 12:24:35 +0530

    data types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,7 +1,7 @@
 "user strict" // treat all js code as newer version
 //alert(3+3) // we are using node js not browser
 
-//primitive data type 
+// ************** primitive data type(call by value)  == 7 ***********
  
 //number : use for number
 let age = 20;
@@ -25,8 +25,14 @@ let bigNumber = 68268009463n;
 
 //Symbol : used to create unique identifiers
 let id = Symbol("id");
+
+
+
  
-//  Non-primitive (Reference) data type
+// *****************Non-primitive (Reference) data type ***********
+
+
+
 
 //Object : store multiple values together
 let student ={
@@ -48,3 +54,9 @@ function greet(){
 console.log(typeof"Rashi");
 console.log(typeof age);
 console.log(typeof null); //returns Object
+
+
+//*********** Type of Javascript ********
+//javascript is a dynamically typed language because variable types are deytermined at runtime and can change during execution
+//Dynamib Type : you do not neet to declear tha Data Type of the Variable
+//Static type : the type must be Declear and cannot change
