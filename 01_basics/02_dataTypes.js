4e0c20c0c9b713956a6e6f38a61f2addd8dd3b7b
arpitaverma28 <arpitaverma13578@gmail.com>
2026-03-15 12:45:18 +0530

    data types and memory

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -60,3 +60,66 @@ console.log(typeof null); //returns Object
 //javascript is a dynamically typed language because variable types are deytermined at runtime and can change during execution
 //Dynamib Type : you do not neet to declear tha Data Type of the Variable
 //Static type : the type must be Declear and cannot change
+
+//++++++++++ Memory *******************
+
+/*
+Memory in JavaScript means how JavaScript stores and manages data in RAM (computer memory) while the program runs.
+
+JavaScript mainly uses two types of memory areas:
+
+1️⃣ Stack Memory
+Stack memory is used for primitive data types.
+Examples of primitive types:
+Number,String,Boolean,Null,Undefined,Symbol,BigInt
+
+Example
+let a = 10;
+let b = a;
+b = 20;
+
+console.log(a); // 10
+console.log(b); // 20
+Explanation
+a stores 10 in stack memory
+b gets a copy of a
+When b changes to 20, a does not change
+✔ Stack stores actual values.
+
+2️⃣ Heap Memory
+Heap memory is used for reference data types.
+Examples of reference types:
+Object,Array,Function
+
+Example
+let obj1 = {name: "Arpita"};
+let obj2 = obj1;
+
+obj2.name = "Star";
+
+console.log(obj1.name); // Star
+console.log(obj2.name); // Star
+
+Explanation
+Object is stored in heap memory
+obj1 stores the reference (address)
+obj2 gets the same reference
+Changing obj2 also changes obj1
+
+✔ Heap stores objects and complex data.
+
+📊 Simple Difference
+Feature	            Stack	                   Heap
+Used for    	Primitive types         	Objects, arrays
+Stores	        Actual value	           Reference (address)
+Speed          	Faster	                   Slower
+Example	        let a = 10	               let obj = {}
+
+🧠 Easy Way to Remember
+Stack → Copy value
+Heap → Copy reference (address)
+
+⭐ one-line answer:
+
+In JavaScript, primitive values are stored in stack memory, while objects and arrays are stored in heap memory, and variables hold references to them  
+*/
